2c606fc9f22a0e7fe7199face6f4c964b2672930
erick uluac mijangos <uluac@hotmail.com>
2012-11-10 14:03:50 -0600

    cambio acel 2
    se ingreso una tabla para desplegar los valores x,y,z,t

--- js/actions.js
+++ js/actions.js
@@ -35,10 +35,10 @@ function docReady() {
 		},false);
 
 		function onSuccess(acceleration) {
-		    alert('Acceleration X: ' + acceleration.x + '\n' +
-        		  'Acceleration Y: ' + acceleration.y + '\n' +
-		          'Acceleration Z: ' + acceleration.z + '\n' +
-        		  'Timestamp: '      + acceleration.timestamp + '\n');
+			posX.innerHTML=acceleration.x;
+			posY.innerHTML=acceleration.y;
+			posZ.innerHTML=acceleration.z;
+        	posT.innerHTML=acceleration.timestamp;
 		};
 
 		function onError() {
