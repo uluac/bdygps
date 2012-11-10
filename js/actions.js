470fc42889fa2e4459743ae2e0ddc8d44898fdda
erick uluac mijangos <uluac@hotmail.com>
2012-11-10 13:50:32 -0600

    se agrego acelerometro

--- js/actions.js
+++ js/actions.js
@@ -33,5 +33,20 @@ function docReady() {
 				});
 			} 
 		},false);
+
+		function onSuccess(acceleration) {
+		    alert('Acceleration X: ' + acceleration.x + '\n' +
+        		  'Acceleration Y: ' + acceleration.y + '\n' +
+		          'Acceleration Z: ' + acceleration.z + '\n' +
+        		  'Timestamp: '      + acceleration.timestamp + '\n');
+		};
+
+		function onError() {
+		    alert('onError!');
+		};
+
+		var options = { frequency: 3000 };  // Update every 3 seconds
+		var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+
 	},false);
 }
